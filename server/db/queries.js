c2067ab6d63845c033865347eebe866bb56f5ea2
Daniel Jordan <danieljordan86@gmail.com>
2020-01-15 11:27:57 -0700

    Uses Post to write objects to db

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,40 +1,28 @@
 // create db
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bugreports', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/bugreports', {useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', () => console.log('database connection error'));
 db.once('open', () => console.log('database up and running'));
 
 const BugSchema = new mongoose.Schema({
   bugName: Number,
   bugDescription: String,
   reportedBy: String,
-  createdDate: { type: Date, default: Date.now },
+  createdDate: { type: Date, default: Date.now() },
   assignedTo: String,
   threatLevel: String,
 });
 
 const Report = mongoose.model('Report', BugSchema);
 
-module.exports = Report;
+module.exports.Report = Report;
 
 
 
 
-
-
-/*
-{
-    bugName: '2',
-    bugDescription: 'Do you support the Phantom Thieves?',
-    reportedBy: 'Jeff',
-    createdDate: '1/5/2020',
-    assignedTo: 'Daniel',
-    threatLevel: 'Low-Priority',
-  }
-*/
 
 
 
--- server/index.js
+++ server/index.js
@@ -2,27 +2,32 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { db, Report } = require('./db/queries');
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
 //GET route to send all bug reports to user from database
+//find()
 app.get('/', (req, res) => {
-  console.log('get all reports route')
+
   res.send();
 })
 
 //POST method to add new bug report to database
+//save()
 app.post('/', (req, res) => {
-  console.log('post new report route')
-  res.send();
+  const report = new Report(req.body);
+  report.save()
+  .then(() => res.status(200).send(reporty))
+  .catch(err => err);
 })
-
 
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
 })
-//
+
+
+
