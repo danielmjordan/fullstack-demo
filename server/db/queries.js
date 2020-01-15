7aad4a0a80793ec55da782f363c31b358f38825d
Daniel Jordan <danieljordan86@gmail.com>
2020-01-15 10:39:23 -0700

    Creates mongoose db with schema and model

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,1 +1,41 @@
 // create db
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/bugreports', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('database up and running'));
+
+const BugSchema = new mongoose.Schema({
+  bugName: Number,
+  bugDescription: String,
+  reportedBy: String,
+  createdDate: { type: Date, default: Date.now },
+  assignedTo: String,
+  threatLevel: String,
+});
+
+const Report = mongoose.model('Report', BugSchema);
+
+module.exports = Report;
+
+
+
+
+
+
+/*
+{
+    bugName: '2',
+    bugDescription: 'Do you support the Phantom Thieves?',
+    reportedBy: 'Jeff',
+    createdDate: '1/5/2020',
+    assignedTo: 'Daniel',
+    threatLevel: 'Low-Priority',
+  }
+*/
+
+
+
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+//GET route to send all bug reports to user from database
+app.get('/', (req, res) => {
+  console.log('get all reports route')
+  res.send();
+})
+
+//POST method to add new bug report to database
+app.post('/', (req, res) => {
+  console.log('post new report route')
+  res.send();
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+})
+//
